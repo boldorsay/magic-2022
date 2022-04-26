68a009d27d5209e579ec4593511c508e9f6739cb
boldorsay <72067985+boldorsay@users.noreply.github.com>
2022-04-26 10:36:26 +0200

    rotation shape

---
+++ Assets/MainObjectFeedbackRotation.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Formats.Alembic.Importer;
+
+public class MainObjectFeedbackRotation : MonoBehaviour
+{
+    public InputAction encoder;
+    Vector3 scaleVel;
+
+
+    void Awake()
+    {
+        encoder.Enable();
+
+
+    }
+
+    void Update()
+    {
+        GetComponent<AlembicStreamPlayer>().CurrentTime = encoder.ReadValue<float>() * 10000;
+        // timePaul.GetComponent<AlembicStreamPlayer>().CurrentTime = encoder1.ReadValue<float>() * 10000;
+        //transform.rotation = Quaternion.Euler(0,encoder1.ReadValue<float>()*10000,encoder2.ReadValue<float>()*10000);
+        // Vector3 targetScale = Vector3.one * Mathf.Lerp(.1f,.2f, button.ReadValue<float>());
+        // transform.localScale = Vector3.SmoothDamp(transform.localScale, targetScale, ref scaleVel, 0.2f);
+
+    }
+}
+
